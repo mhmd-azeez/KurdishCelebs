f50f76f39e8bac7e1be3dea1f11e36162b821783
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-04-04 13:44:13 +0300

    some minor improvements

--- KurdishCelebs.WebApp/wwwroot/js/site.js
+++ KurdishCelebs.WebApp/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$.fn.addDownloadBtn = function (
+    text,
+    className
+) {
+    var el = $(this);
+
+    if (el.is("img")) {
+        var tagLink = "<a>";
+        var tagDiv = "<div>";
+
+        var cssDiv = {
+            display: "inline-block",
+            position: "relative"
+        };
+        var cssLink = {
+            position: "absolute",
+            top: "10px",
+            right: "10px"
+        };
+
+        var elDiv = $(tagDiv).css(cssDiv);
+
+        var elImg = el.clone();
+        var elImgSrc = el.attr("src");
+
+        var elLink = $(tagLink)
+            .attr("href", $("img").attr("src"))
+            .attr("download", "")
+            .text(text)
+            .addClass(className)
+            .css(cssLink);
+
+        var all = elDiv.append(elImg).append(elLink);
+
+        $("img").replaceWith(all);
+
+        return elLink;
+    }
+};
