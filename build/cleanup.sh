772cfdb2b5a785f3a2d410c129c29891f8f4b753
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-04-06 22:26:52 +0300

    only upload publish folder

--- build/cleanup.sh
+++ build/cleanup.sh
@@ -1,3 +1,3 @@
 # Remove the models and native libraries since they are very big and we already have them on the server
-rm KurdishCelebs.WebApp/bin/x64/Release/netcoreapp3.1/linux-x64/models -r
-rm KurdishCelebs.WebApp/bin/x64/Release/netcoreapp3.1/linux-x64/lib*.so
+rm KurdishCelebs.WebApp/bin/x64/Release/netcoreapp3.1/linux-x64/publish/models -r
+rm KurdishCelebs.WebApp/bin/x64/Release/netcoreapp3.1/linux-x64/publish/lib*.so
